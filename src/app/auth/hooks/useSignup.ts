5290705a008209eb854dadebfa9752de9e6501bc
sujan <sujanmaharjna@bibhutisolutions.com>
2025-04-15 13:24:43 +0545

    build error fixes

--- src/app/auth/hooks/useSignup.ts
+++ src/app/auth/hooks/useSignup.ts
@@ -3,6 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 import { axiosInstance } from '@/lib/axiosInstance';
 import { useAuthStore } from '@/stores/authStore';
 import { queryClient } from '@/lib/queryClient';
+import { SignUpParams } from '@/types';
 
 export const useSignup = () => {
 
